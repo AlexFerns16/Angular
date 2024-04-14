029f1779baf4e2f50b86364b182c77514e27f016
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-04-14 17:08:56 +0530

    commit:8 | Using Custom Components

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -8,6 +9,7 @@ import { ServerComponent } from './server/server.component';
 @NgModule({
   declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule],
+  // imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
